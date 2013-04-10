ea93642b314fad6f4c1190a936cfe8a99fd1b8ad
Yogesh Girdhar <yogesh@cim.mcgill.ca>
2013-04-10 09:23:55 -0400

    added arm/disarm service

--- nodes/roscopter.py
+++ nodes/roscopter.py
@@ -2,6 +2,7 @@
 import roslib; roslib.load_manifest('roscopter')
 import rospy
 from std_msgs.msg import String, Header
+from std_srvs.srv import *
 from sensor_msgs.msg import NavSatFix, NavSatStatus, Imu
 import roscopter.msg
 import sys,struct,time,os
@@ -13,8 +14,8 @@ from optparse import OptionParser
 parser = OptionParser("roscopter.py [options]")
 
 parser.add_option("--baudrate", dest="baudrate", type='int',
-                  help="master port baud rate", default=115200)
-parser.add_option("--device", dest="device", default=None, help="serial device")
+                  help="master port baud rate", default=57600)
+parser.add_option("--device", dest="device", default="/dev/ttyUSB0", help="serial device")
 parser.add_option("--rate", dest="rate", default=10, type='int', help="requested stream rate")
 parser.add_option("--source-system", dest='SOURCE_SYSTEM', type='int',
                   default=255, help='MAVLink source system for this GCS')
@@ -38,17 +39,37 @@ def wait_heartbeat(m):
     print("Heartbeat from APM (system %u component %u)" % (m.target_system, m.target_system))
 
 
-def mav_control(data):
-    master.mav.set_roll_pitch_yaw_thrust_send(master.target_system, master.target_component,
-                                                                 data.roll, data.pitch, data.yaw, data.thrust)
-
-    print ("sending control: %s"%data)
+#This does not work yet because APM does not have it implemented
+#def mav_control(data):
+#    '''
+#    Set roll, pitch and yaw.
+#    roll                      : Desired roll angle in radians (float)
+#    pitch                     : Desired pitch angle in radians (float)
+#    yaw                       : Desired yaw angle in radians (float)
+#    thrust                    : Collective thrust, normalized to 0 .. 1 (float)
+#    '''    
+#    master.mav.set_roll_pitch_yaw_thrust_send(master.target_system, master.target_component,
+#                                                                data.roll, data.pitch, data.yaw, data.thrust)
+#
+#    print ("sending control: %s"%data)
 
 
 def send_rc(data):
     master.mav.rc_channels_override_send(master.target_system, master.target_component,data.channel[0],data.channel[1],data.channel[2],data.channel[3],data.channel[4],data.channel[5],data.channel[6],data.channel[7])
     print ("sending rc: %s"%data)
 
+
+#service callbacks
+#def set_mode(mav_mode):
+#    master.set_mode_auto()
+
+def set_arm(req):
+    master.arducopter_arm()
+    return True
+
+def set_disarm(req):
+    master.arducopter_disarm()
+    return True
 
 pub_gps = rospy.Publisher('gps', NavSatFix)
 #pub_imu = rospy.Publisher('imu', Imu)
@@ -58,10 +79,18 @@ pub_vfr_hud = rospy.Publisher('vfr_hud', roscopter.msg.VFR_HUD)
 pub_attitude = rospy.Publisher('attitude', roscopter.msg.Attitude)
 pub_raw_imu =  rospy.Publisher('raw_imu', roscopter.msg.Mavlink_RAW_IMU)
 if opts.enable_control:
-    rospy.Subscriber("control", roscopter.msg.Control , mav_control)
+    #rospy.Subscriber("control", roscopter.msg.Control , mav_control)
     rospy.Subscriber("send_rc", roscopter.msg.RC , send_rc)
 
+#define service callbacks
+arm_service = rospy.Service('arm',Empty,set_arm)
+disarm_service = rospy.Service('disarm',Empty,set_disarm)
+
+
+#state
 gps_msg = NavSatFix()
+
+
 
 def mainloop():
     rospy.init_node('roscopter')
